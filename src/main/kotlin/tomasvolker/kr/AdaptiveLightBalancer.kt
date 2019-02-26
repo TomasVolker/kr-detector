df8447fbc6de76a70e4904810984bc67af8015a9
Agustin-Picard <agustin.picard@gmail.com>
2019-02-26 13:55:01 +0100

    Continued on the adaptive

--- src/main/kotlin/tomasvolker/kr/AdaptiveLightBalancer.kt
+++ src/main/kotlin/tomasvolker/kr/AdaptiveLightBalancer.kt
@@ -5,7 +5,7 @@ import tomasvolker.numeriko.core.operations.unstack
 import kotlin.math.absoluteValue
 
 
-//fun <T> List<T>.split(times: Int) =
+fun <T> List<T>.split(times: Int) =
 
 class AdaptiveLightBalancer(val sectionCount: Int,
                             val maxCount: Int,
@@ -28,6 +28,14 @@ class AdaptiveLightBalancer(val sectionCount: Int,
                     0.0
                 else
                     (sectionLevels[i][j].first - sectionLevels[i][j - 1].first).absoluteValue) to sectionLevels[i][j]
+            }
+        }.map {
+            it.map {
+                if (it.first > diffThreshold) {
+                    val aux = it.second to sectionLevels.indexOf(it.second)
+                    sectionLevels.removeAt(aux.second)
+                    sectionLevels.addAll(aux.second, it.second.)
+                }
             }
         }
 

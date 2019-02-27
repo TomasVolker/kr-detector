c670e521c9177ca40c7fd92820138cb4401299f0
Agustin-Picard <agustin.picard@gmail.com>
2019-02-27 12:22:37 +0100

    Tidy up the code a bit

--- src/main/kotlin/tomasvolker/kr/QRPatternFinder.kt
+++ src/main/kotlin/tomasvolker/kr/QRPatternFinder.kt
@@ -1,95 +1,121 @@
 package tomasvolker.kr
 
+import boofcv.alg.filter.binary.GThresholdImageOps
+import boofcv.factory.fiducial.FactoryFiducial
+import boofcv.gui.binary.VisualizeBinaryData
+import boofcv.io.image.ConvertBufferedImage
+import boofcv.io.webcamcapture.UtilWebcamCapture
+import boofcv.struct.ConfigLength
+import boofcv.struct.image.GrayF32
+import boofcv.struct.image.GrayU8
 import com.github.tomasvolker.parallel.mapParallel
+import org.openrndr.application
+import org.openrndr.color.ColorRGBa
+import org.openrndr.draw.colorBuffer
+import tomasvolker.kr.openrndr.write
 import tomasvolker.numeriko.core.dsl.I
 import tomasvolker.numeriko.core.functions.transpose
 import tomasvolker.numeriko.core.interfaces.array1d.double.DoubleArray1D
 import tomasvolker.numeriko.core.interfaces.array1d.integer.IntArray1D
 import tomasvolker.numeriko.core.interfaces.array2d.double.DoubleArray2D
+import tomasvolker.numeriko.core.interfaces.array2d.generic.forEachIndex
+import tomasvolker.numeriko.core.interfaces.factory.doubleArray2D
 import tomasvolker.numeriko.core.operations.stack
 import tomasvolker.numeriko.core.operations.unstack
 
 
-fun Int.inTolerance(center: Int, tolerance: Double) =
-    (this > center - center * tolerance) && (this < center + center * tolerance)
-
-data class QRPattern(val x: Int, val y: Int, val unit: Int)
-
-fun QRPattern.invertAxis() = QRPattern(y, x, unit)
-
-fun QRPattern.isSame(other: QRPattern) =
-    (unit == other.unit &&
-            (other.x - 1 == x || other.x + 1 == x && other.y == y) ||
-            (other.y - 1 == y || other.y + 1 == y && other.x == x))
-
-fun QRPattern.center(axis: Int = 0) =
-    if (axis == 0) QRPattern(x - 1 * unit, y, unit) else QRPattern(x, y - 1 * unit, unit)
-
-data class LineSection(val value: Boolean, val rangeX: IntArray1D, val rangeY: IntArray1D)
-
-val LineSection.unit get() = if (rangeX.size != 1) rangeX[1] - rangeX[0] else rangeY[1] - rangeY[0]
-
-val LineSection.centerX get() = if (rangeX.size > 1) (rangeX[0] + rangeX[1]) / 2 else rangeX[0]
-
-val LineSection.centerY get() = if (rangeY.size > 1) (rangeY[0] + rangeY[1]) / 2 else rangeY[0]
-
-fun LineSection.invertAxis() = LineSection(value, rangeY, rangeX)
-
-fun LineSection.isNeighbor(other: LineSection) =
-    (rangeX[0] == other.rangeX[1]) || (rangeX[1] == other.rangeX[0]) ||
-            (rangeY[0] == other.rangeY[1]) || (rangeY[1] == other.rangeY[0])
-
-fun List<LineSection>.isPattern(tolerance: Double) =
-    !this[0].value && this[1].unit.inTolerance(this[0].unit, tolerance)
-            && this[3].unit.inTolerance(this[0].unit, tolerance)
-            && this[4].unit.inTolerance(this[0].unit, tolerance)
-            && this[2].unit.inTolerance(this[0].unit * 3, tolerance)
-
-
-fun DoubleArray1D.toLineSections(from: Int = 0, axisIndex: Int = 0): List<LineSection> {
-    return if (size == 0)
-        emptyList()
-    else {
-        indexOfFirst { it != this[0] }.let {
-            listOf(LineSection(this[0] == 1.0,
-                I[from,it], I[axisIndex])) + this[it until size].toLineSections(it, axisIndex)
-        }
-    }
-}
-
-fun <T> List<T>.interleaved(overlap: Int): Sequence<List<T>> = sequence {
-    for (i in 0 until size - overlap) {
-        yield(subList(i, i + overlap))
-    }
-}
-
 class QRPatternFinder() {
+    companion object {
+        fun findPatterns(image: DoubleArray2D, tolerance: Double = 0.2): List<QRPattern> {
+            val lines = image.unstack()
 
-    fun findPatterns(image: DoubleArray2D, tolerance: Double = 0.2): List<QRPattern> {
-        val lines = image.unstack()
-
-        return scanImageHorizontal(lines, tolerance).let { horizontal ->
-            horizontal + scanImageVertical(lines, tolerance).filter { !horizontal.contains(it) }
+            return scanImageHorizontal(lines, tolerance).let { horizontal ->
+                horizontal + scanImageVertical(lines, tolerance).filter { !horizontal.contains(it) }
+            }
         }
+
+        private fun scanImageHorizontal(lines: List<DoubleArray1D>, tolerance: Double = 0.2): List<QRPattern> {
+            val qrPatternList = mutableListOf<QRPattern>()
+
+            lines.mapIndexed { index, line ->
+                line.toLineSections(axisIndex = index)
+                    .interleaved(overlap = 4)
+                    .map {
+                        if (it.isPattern(tolerance))
+                            qrPatternList.add(QRPattern(it[2].centerX, it[2].centerY, it[0].unit))
+                    }
+            }
+
+            return qrPatternList.filterIndexed { index, qrPattern ->
+                !qrPattern.isSame(qrPatternList[index + 1])
+            }.map { it.center(axis = 1) }
+        }
+
+        private fun scanImageVertical(lines: List<DoubleArray1D>, tolerance: Double = 0.2): List<QRPattern> =
+            scanImageHorizontal(lines.stack().transpose().unstack(), tolerance).map { it.invertAxis() }
     }
+}
 
-    private fun scanImageHorizontal(lines: List<DoubleArray1D>, tolerance: Double = 0.2): List<QRPattern> {
-        val qrPatternList = mutableListOf<QRPattern>()
 
-        lines.mapIndexed { index, line ->
-            line.toLineSections(axisIndex = index)
-                .interleaved(overlap = 4)
-                .map {
-                    if (it.isPattern(tolerance))
-                        qrPatternList.add(QRPattern(it[2].centerX, it[2].centerY, it[0].unit))
+fun DoubleArray2D.toGrayF32(): GrayF32 =
+        GrayF32(shape0, shape1).also { this.forEachIndex { i0, i1 ->
+            it.set(i0, i1, this[i0,i1].toFloat())
+        } }
+
+fun GrayF32.toDoubleArray2D(): DoubleArray2D =
+        doubleArray2D(width, height) { i0, i1 -> get(i0, i1) }
+
+
+fun main() {
+    val webcam = UtilWebcamCapture.openDefault(640, 480)
+
+    application {
+
+        configure {
+            windowResizable = true
+        }
+
+        program {
+
+            val buffer = colorBuffer(640, 480)
+            val binary = GrayU8(webcam.viewSize.width, webcam.viewSize.height)
+
+            extend(PanZoom())
+
+            extend {
+
+                val image = webcam.image
+                val input = ConvertBufferedImage.convertFromSingle(image, null, GrayF32::class.java)
+
+                GThresholdImageOps.localMean(input, binary, ConfigLength.fixed(57.0), 1.0, true, null, null)
+
+                val qrPatternList = QRPatternFinder.findPatterns(input.toDoubleArray2D(), tolerance = 0.2)
+
+                val gray = VisualizeBinaryData.renderBinary(binary, false, null)
+
+                buffer.write(gray)
+                drawer.image(buffer)
+                drawer.stroke = ColorRGBa.GREEN
+                drawer.strokeWeight = 2.0
+
+                drawer.run {
+
+                    fill = ColorRGBa.RED
+
+                    qrPatternList.forEach {
+                        println(it)
+                        circle(x = it.x.toDouble(), y = it.y.toDouble(), radius = it.unit / 2.0)
+                    }
+
                 }
+
+            }
+
         }
 
-        return qrPatternList.filterIndexed { index, qrPattern ->
-            !qrPattern.isSame(qrPatternList[index + 1])
-        }.map { it.center(axis = 1) }
+
     }
 
-    private fun scanImageVertical(lines: List<DoubleArray1D>, tolerance: Double = 0.2): List<QRPattern> =
-        scanImageHorizontal(lines.stack().transpose().unstack(), tolerance).map { it.invertAxis() }
+    webcam.close()
+
 }
